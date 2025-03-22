4fd7c177d70cbd182ae71603281123ef91129512
Qianqian Fang <fangqq@gmail.com>
2025-03-21 23:32:59 -0400

    [build] allow to build on windows and mac with libhdf5 1.12+

--- src/+H5O/get_info_by_name.cc
+++ src/+H5O/get_info_by_name.cc
@@ -65,7 +65,11 @@ num_attrs; /* # of attributes attached to object \n\
   // Build ouput structure
   octave_scalar_map info_struct;
   info_struct.assign ("fileno", octave_int64 (oinfo.fileno));
+
+#if ((H5_VERS_MAJOR * 1000) + H5_VERS_MINOR) <= 1010
   info_struct.assign ("addr", octave_int64 (oinfo.addr));
+#endif
+
   info_struct.assign ("type", octave_int64 (oinfo.type));
   info_struct.assign ("rc", octave_int64 (oinfo.rc));
   info_struct.assign ("atime", octave_int64 (oinfo.atime));
